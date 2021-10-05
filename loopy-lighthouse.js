0b38418d02d81fa9db7c84235fade1ce77900fd5
Bles Mateo <89666942+BlesMateo@users.noreply.github.com>
2021-10-05 12:20:59 -0400

    Add files via upload

---
+++ loopy-lighthouse.js
@@ -0,0 +1,11 @@
+var x = 100;
+for (var x = 100; x <= 200; x = x + 1 ) {
+    if (x % 3 === 0) {console.log("Loopy");
+    } else if { (x % 4 === 0) {
+        console.log("Lighthouse");
+    } else if { (x % 3 && x % 4) {
+        console.log("LoopyLighthouse");
+    }
+    }
+        console.log(x);
+    }
